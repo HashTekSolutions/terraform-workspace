e509b776cb7ce6cd514099037670d41329e63959
HashTekSolutions <113618701+HashTekSolutions@users.noreply.github.com>
2023-06-26 13:12:10 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    profile = tfuser
+    
     bucket  = "hashtek-terraform-workspace" # Replace with your S3 bucket name
     key     = "statefile/terraform.tfstate"
     region  = "ap-south-1"
