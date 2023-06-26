55633c2e29d2722566c1d81a0a99671922a219c8
HashTekSolutions <113618701+HashTekSolutions@users.noreply.github.com>
2023-06-26 12:26:01 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,11 @@
 provider "aws" {
   region  = "ap-south-1"
-  profile = "sai"
+
 }
 
 terraform {
   backend "s3" {
-    profile = "sai"
-    bucket  = "manohar-terraform-code-9966" # Replace with your S3 bucket name
+    bucket  = "hashtek-terraform-workspace" # Replace with your S3 bucket name
     key     = "statefile/terraform.tfstate"
     region  = "ap-south-1"
   }
