609a24558258b94a78036a481d246008a153030a
HashTekSolutions <113618701+HashTekSolutions@users.noreply.github.com>
2023-06-27 22:30:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami                    = "ami-057752b3f1d6c4d6c" # Replace with your desired AMI ID
   instance_type          = "t2.micro"
-  key_name   = "${terraform.workspace}-${var.keypair}" # Replace with your key pair name
+  key_name   = "${terraform.workspace}-${var.keypair}-${random_integer.ri.result}" # Replace with your key pair name
   vpc_security_group_ids = [aws_security_group.sg_web.id]
 user_data = <<-EOF
               #!/bin/bash
