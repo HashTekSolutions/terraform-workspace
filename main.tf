b5fbb5baf92f891c230d57269d52a9a1f69872a4
sasikanth53 <42739097+sasikanth53@users.noreply.github.com>
2023-07-04 19:50:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami                    = "ami-057752b3f1d6c4d6c" # Replace with your desired AMI ID
-  instance_type          = "t2.micro"
+  instance_type          =  var.instance_type
   key_name               =  aws_key_pair.deployer.key_name # Replace with your key pair name
   vpc_security_group_ids = [aws_security_group.sg_web.id]
 user_data = <<-EOF
